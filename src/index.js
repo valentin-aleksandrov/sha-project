e1b497ff2a88d8141e999e33cf4cca6dc5dada97
Valentin Aleksandrov <valentin.aleksandrov@tick42.com>
2020-04-25 17:05:24 +0300

    complete message pad

--- src/index.js
+++ src/index.js
@@ -1,6 +1,46 @@
 "use strict";
 exports.__esModule = true;
-var fileSystem = require("fs");
+// import * as fileSystem from 'fs';
+var ramda_1 = require("ramda");
+exports.getAsciiValue = function (character) {
+    return character.charCodeAt(0);
+};
+exports.toBinary = function (number) { return number.toString(2); };
+exports.normalizeToEightBits = function (binaryValue) {
+    return binaryValue.length === 8
+        ? binaryValue
+        : exports.normalizeToEightBits('0' + binaryValue);
+};
+exports.normalizeToOneHundredTwentyEightBits = function (binaryValue) {
+    return binaryValue.length === 128
+        ? binaryValue
+        : exports.normalizeToOneHundredTwentyEightBits('0' + binaryValue);
+};
+exports.characterToBinary = ramda_1.compose(exports.normalizeToEightBits, exports.toBinary, exports.getAsciiValue);
+exports.toOneHundredTwentyEightBitsBinary = ramda_1.compose(exports.normalizeToOneHundredTwentyEightBits, exports.toBinary);
+exports.convertWordToBits = function (word) {
+    return word
+        .split('')
+        .map(exports.characterToBinary)
+        .join('');
+};
+exports.zerosCountInMessagePreparing = function (messageLength) {
+    return 896 - messageLength - 1;
+};
+var getMessageModThousandTwentyFour = function (message, zeroes, lastPart) {
+    var newMessage = message + '1' + zeroes + lastPart;
+    return newMessage.length % 1024 === 0
+        ? newMessage
+        : getMessageModThousandTwentyFour(message, zeroes + '0', lastPart);
+};
+exports.padMessage = function (message) {
+    return message.length < 896
+        ? message +
+            '1' +
+            '0'.repeat(exports.zerosCountInMessagePreparing(message.length)) +
+            exports.toOneHundredTwentyEightBitsBinary(message.length)
+        : getMessageModThousandTwentyFour(message, '0', exports.toOneHundredTwentyEightBitsBinary(message.length));
+};
 exports.application = function () {
     //   const stdin = process.openStdin();
     //   stdin.addListener('data', function(d) {
@@ -9,7 +49,10 @@ exports.application = function () {
     //     // with toString() and then substring()
     //     console.log('you entered: [' + d.toString().trim() + ']');
     //   });
-    var dataFromFile = fileSystem.readFileSync('C:\\Users\\VALKA\\Desktop\\VVPS_PROJECT\\repositories\\sha-project\\textFile.txt', 'utf8');
-    console.log('text from the file', dataFromFile);
-    console.log('Greetings, people!');
+    //   const dataFromFile = fileSystem.readFileSync(
+    //     'C:\\Users\\VALKA\\Desktop\\VVPS_PROJECT\\repositories\\sha-project\\textFile.txt',
+    //     'utf8',
+    //   );
+    //   console.log('text from the file', dataFromFile);
+    console.log(exports.padMessage(exports.convertWordToBits('abc')).length);
 };
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,71 @@
-import * as fileSystem from 'fs';
+// import * as fileSystem from 'fs';
+import { compose, last } from 'ramda';
+
+export const getAsciiValue = (character: string): number => {
+  return character.charCodeAt(0);
+};
+
+export const toBinary = (number: number): string => number.toString(2);
+
+export const normalizeToEightBits = (binaryValue: string): string => {
+  return binaryValue.length === 8
+    ? binaryValue
+    : normalizeToEightBits('0' + binaryValue);
+};
+
+export const normalizeToOneHundredTwentyEightBits = (
+  binaryValue: string,
+): string => {
+  return binaryValue.length === 128
+    ? binaryValue
+    : normalizeToOneHundredTwentyEightBits('0' + binaryValue);
+};
+
+export const characterToBinary = compose(
+  normalizeToEightBits,
+  toBinary,
+  getAsciiValue,
+);
+
+export const toOneHundredTwentyEightBitsBinary = compose(
+  normalizeToOneHundredTwentyEightBits,
+  toBinary,
+);
+
+export const convertWordToBits = (word: string): string => {
+  return word
+    .split('')
+    .map(characterToBinary)
+    .join('');
+};
+
+export const zerosCountInMessagePreparing = (messageLength: number): number => {
+  return 896 - messageLength - 1;
+};
+
+const getMessageModThousandTwentyFour = (
+  message: string,
+  zeroes: string,
+  lastPart: string,
+): string => {
+  const newMessage = message + '1' + zeroes + lastPart;
+  return newMessage.length % 1024 === 0
+    ? newMessage
+    : getMessageModThousandTwentyFour(message, zeroes + '0', lastPart);
+};
+
+export const padMessage = (message: string): string => {
+  return message.length < 896
+    ? message +
+        '1' +
+        '0'.repeat(zerosCountInMessagePreparing(message.length)) +
+        toOneHundredTwentyEightBitsBinary(message.length)
+    : getMessageModThousandTwentyFour(
+        message,
+        '0',
+        toOneHundredTwentyEightBitsBinary(message.length),
+      );
+};
 
 export const application = (): void => {
   //   const stdin = process.openStdin();
@@ -8,10 +75,11 @@ export const application = (): void => {
   //     // with toString() and then substring()
   //     console.log('you entered: [' + d.toString().trim() + ']');
   //   });
-  const dataFromFile = fileSystem.readFileSync(
-    'C:\\Users\\VALKA\\Desktop\\VVPS_PROJECT\\repositories\\sha-project\\textFile.txt',
-    'utf8',
-  );
-  console.log('text from the file', dataFromFile);
-  console.log('Greetings, people!');
+  //   const dataFromFile = fileSystem.readFileSync(
+  //     'C:\\Users\\VALKA\\Desktop\\VVPS_PROJECT\\repositories\\sha-project\\textFile.txt',
+  //     'utf8',
+  //   );
+  //   console.log('text from the file', dataFromFile);
+
+  console.log(padMessage(convertWordToBits('abc')).length);
 };
